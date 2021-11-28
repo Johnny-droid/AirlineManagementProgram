1de2a15ac67f5aec0eaee4008fcbbb61c9058d6a
Johnny-droid <joaoricardoralves@gmail.com>
2021-11-28 16:26:47 +0000

    First commit

---
+++ cmake-build-debug/CMakeFiles/AirlineManagementProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AirlineManagementProgram"
+  "AirlineManagementProgram.pdb"
+  "CMakeFiles/AirlineManagementProgram.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirlineManagementProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    std::cout << "AED 2021/2022 - Aula Pratica 4" << std::endl;
+    return RUN_ALL_TESTS();
+}
